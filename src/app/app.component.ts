5eb59fb59ca556f286cc673e14bed47b2dbc95ef
Shubham Tiwari <shubhamtiwari@Shubhams-Mac-mini.local>
2024-12-17 23:33:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { EditorComponent } from 'ngx-monaco-editor-v2';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, EditorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent{
+  iframeContent: string = '';
+  outputStream: string[] = [];
   title = 'ngx-code-executor';
+  editorOptions = {
+    language: 'typescript',
+    minimap: { enabled: false },
+    scrollBeyondLastLine: false,
+    lineHeight: 20,
+    fontSize: 14,
+    wordWrap: 'on',
+    wrappingIndent: 'indent',
+    theme: 'vs-dark',
+  };
+  code = '';
+  iframeUrl: any;
+  constructor(private sanitizer: DomSanitizer) {
+    // this.code = this.sanitizer.bypassSecurityTrustHtml(this.code);
+  }
+
+
+  
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { NgxMonacoEditorConfig, provideMonacoEditor } from 'ngx-monaco-editor-v2';
 import { provideRouter } from '@angular/router';
+import { customTheme } from '../theme';
 
 import { routes } from './app.routes';
 
+
+// const monacoConfig: NgxMonacoEditorConfig = {
+//   onMonacoLoad: () => {
+//       // @ts-ignore
+//       window.monaco.editor.defineTheme('customTheme', customTheme);
+//   }
+// };
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideMonacoEditor()]
 };
